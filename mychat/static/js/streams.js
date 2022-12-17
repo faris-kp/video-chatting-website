3c3a478d2654e2c9b7449f7f83494e5bfb97bcfd
Faris <faris46820@gmail.com>
2022-12-17 13:55:50 +0530

    mic and camera toggle done

--- mychat/static/js/streams.js
+++ mychat/static/js/streams.js
@@ -74,6 +74,29 @@ let leaveAndRemoveLocalStream = async () =>{
     window.open('/','_self')
 }
 
+let toggleCamera = async (e) =>{
+    if(LocalTracks[1].muted){
+        await LocalTracks[1].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+    }
+    else{
+        await LocalTracks[1].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
+    }
+}
+let toggleMic = async (e) =>{
+    if(LocalTracks[0].muted){
+        await LocalTracks[0].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+    }
+    else{
+        await LocalTracks[0].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
+    }
+}
+
 JoinAndDisplayLocalStream()
 document.getElementById('leave-btn').addEventListener('click',leaveAndRemoveLocalStream)
+document.getElementById('camera-btn').addEventListener('click',toggleCamera)
+document.getElementById('mic-btn').addEventListener('click',toggleMic)
 
